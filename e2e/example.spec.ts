d023f0e45cb54b773490b3c651614ef1dd88d5ea
sub <sub@mail.com>
2026-01-31 05:18:37 +0000

    Update Playwright test to target our application

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:3000');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Next.js/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('displays hello next message', async ({ page }) => {
+  await page.goto('http://localhost:3000');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to display "Hello Next" message
+  await expect(page.getByText('Hello Next')).toBeVisible();
 });
